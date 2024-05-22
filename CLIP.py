8b6b504a6672120cd765c8db2c4253d619ea2adf
gyubo <gyuboone@naver.com>
2024-05-22 21:43:45 +0900

    model 전체 완료

--- CLIP.py
+++ CLIP.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn
+from typing import Tuple, Union
 
 
 #####################  text encoder start  ########################
@@ -67,13 +68,20 @@ class ResidualAttentionBlock(nn.Module):
         self.embed_dim = embed_dim
         self.n_head = n_head
 
-        self.mha = MultiheadAttention(embed_dim=embed_dim, n_head=n_head)
+        # self.mha = MultiheadAttention(embed_dim=embed_dim, n_head=n_head)
+        self.mha = nn.MultiheadAttention(embed_dim=embed_dim, num_heads=n_head)
         self.ln1 = LayerNorm(embed_dim)
         self.mlp = nn.Sequential(nn.Linear(embed_dim, embed_dim * 4),
             QuickGELU(),
             nn.Linear(embed_dim * 4, embed_dim))
         self.ln2 = LayerNorm(embed_dim)
         self.attn_mask = attn_mask
+
+
+    def attention(self, x: torch.Tensor):
+        self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
+        return self.mha(x, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
+
 
     def forward(self, x):
         x += self.mha(self.ln1(x))
@@ -161,7 +169,7 @@ class AttentionPool2d(nn.Module):
         x = torch.cat([x.mean(dim=0, keepdim=True), x], dim=0)  # (HW+1)NC
         # 공식 구현
         # x = x + self.positional_embedding[:, None, :].to(x.dtype)  # (HW+1)NC
-        x += self.positional_embedding.unsqueeze(1).to(x.dtype)      # (HW+1)NC
+        x += self.positional_embedding.unsqueeze(1).to(x.dtype)      # (HW+1)NC   text encoder에서는 (seq_size, batch_size, Embed_dim)으로 넣어주는 것과 정확히 대응됨.
 
         x, _ = F.multi_head_attention_forward(
             query=x[:1], key=x, value=x,    # global AvgPool 한 앞 (1,N,C) tensor를 query로 삼음.
@@ -250,3 +258,129 @@ class ModifiedResNet(nn.Module):
     
 
 ##################### image encoder end #######################
+
+
+######################## CLIP start ###########################
+
+class CLIP(nn.Module):
+    def __init__(self,
+                 embed_dim: int,
+                 # vision
+                 image_resolution: int,
+                 vision_layers: Union[Tuple[int, int, int, int], int],
+                 vision_width: int,
+                 vision_patch_size: int,
+                 # text
+                 context_length: int,
+                 vocab_size: int,
+                 transformer_width: int,
+                 transformer_heads: int,
+                 transformer_layers: int
+                 ):
+        super().__init__()
+
+        self.context_length = context_length
+
+        if isinstance(vision_layers, (tuple, list)):
+            vision_heads = vision_width * 32 // 64
+            self.visual = ModifiedResNet(
+                layers=vision_layers,
+                output_dim=embed_dim,
+                heads=vision_heads,
+                input_resolution=image_resolution,
+                width=vision_width
+            )
+
+        self.transformer = TextTransformer(
+            embed_dim=transformer_width,
+            n_head=transformer_heads,
+            layers=transformer_layers,
+            attn_mask=self.build_attention_mask()
+        )
+
+        self.vocab_size = vocab_size
+        self.token_embedding = nn.Embedding(vocab_size, transformer_width)
+        self.positional_embedding = nn.Parameter(torch.empty(self.context_length, transformer_width))
+        self.ln_final = LayerNorm(transformer_width)
+
+        self.text_projection = nn.Parameter(torch.empty(transformer_width, embed_dim))
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
+        nn.init.normal_(self.token_embedding.weight, std=0.02)
+        nn.init.normal_(self.positional_embedding, std=0.01)
+
+        if isinstance(self.visual, ModifiedResNet):
+            if self.visual.attnpool is not None:
+                std = self.visual.attnpool.c_proj.in_features ** -0.5
+                nn.init.normal_(self.visual.attnpool.q_proj.weight, std=std)
+                nn.init.normal_(self.visual.attnpool.k_proj.weight, std=std)
+                nn.init.normal_(self.visual.attnpool.v_proj.weight, std=std)
+                nn.init.normal_(self.visual.attnpool.c_proj.weight, std=std)
+
+            for resnet_block in [self.visual.layer1, self.visual.layer2, self.visual.layer3, self.visual.layer4]:
+                for name, param in resnet_block.named_parameters():
+                    if name.endswith("bn3.weight"):
+                        nn.init.zeros_(param)
+
+        proj_std = (self.transformer.width ** -0.5) * ((2 * self.transformer.layers) ** -0.5)
+        attn_std = self.transformer.width ** -0.5
+        fc_std = (2 * self.transformer.width) ** -0.5
+        for block in self.transformer.resblocks:
+            nn.init.normal_(block.attn.in_proj_weight, std=attn_std)
+            nn.init.normal_(block.attn.out_proj.weight, std=proj_std)
+            nn.init.normal_(block.mlp.c_fc.weight, std=fc_std)
+            nn.init.normal_(block.mlp.c_proj.weight, std=proj_std)
+
+        if self.text_projection is not None:
+            nn.init.normal_(self.text_projection, std=self.transformer.width ** -0.5)
+
+    def build_attention_mask(self):
+        # lazily create causal attention mask, with full attention between the vision tokens
+        # pytorch uses additive attention mask; fill with -inf
+        mask = torch.empty(self.context_length, self.context_length)
+        mask.fill_(float("-inf"))
+        mask.triu_(1)  # zero out the lower diagonal
+        return mask
+
+    @property
+    def dtype(self):
+        return self.visual.conv1.weight.dtype
+
+    def encode_image(self, image):
+        return self.visual(image.type(self.dtype))       # vision은 이미 embed_dim에 맞게 나오도록 되어 있음
+
+    def encode_text(self, text):
+        x = self.token_embedding(text).type(self.dtype)  # [batch_size, seq_size, transformer_width]  NLD
+
+        x = x + self.positional_embedding.type(self.dtype)
+        x = x.permute(1, 0, 2)  # NLD -> LND             # transformer 안의 nn.MultiheadAttention 에서는 forward할 때 x가 (seq_size, batch_size, transformer_width) 순으로 들어가야 함.
+        x = self.transformer(x)                          # nn.Linear 통과할 때는 마지막 demension인 transformer_width에만 적용되므로 transformer 안에서 변환할 필요는 없음
+        x = x.permute(1, 0, 2)  # LND -> NLD             # 다시 변환
+        x = self.ln_final(x).type(self.dtype)
+
+        # x.shape = [batch_size, n_ctx, transformer.width]
+        # take features from the eot embedding (eot_token is the highest number in each sequence)
+        x = x[torch.arange(x.shape[0]), text.argmax(dim=-1)] @ self.text_projection    # (batch_size, seq_size, transformer_width)의 x 중 마지막 토큰의 벡터만 사용 -> (batch_size, transformer_width)
+                                                                                       # 여기에 @ (transformer_width, 최종 embed_dim)을 곱하므로 최종 x.shape = (batch_size, embed_dim)이 됨.
+                                                                                       # 이로써 encode_img와 차원이 아예 똑같아지고, text와 embedding vector가 일대일 대응됨.
+        return x
+
+    def forward(self, image, text):
+        image_features = self.encode_image(image)
+        text_features = self.encode_text(text)
+
+        # normalized features
+        image_features = image_features / image_features.norm(dim=1, keepdim=True)
+        text_features = text_features / text_features.norm(dim=1, keepdim=True)
+
+        # cosine similarity as logits
+        ## logit_scale : learned temperature parameter
+        logit_scale = self.logit_scale.exp()   ## self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07)) 즉, scalar parameter
+        logits_per_image = logit_scale * image_features @ text_features.t()  ## scalar * (batch_size, embed_dim) @ (embed_dim, batch_size)
+        logits_per_text = logits_per_image.t()
+
+        # shape = [batch_size, batch_size]
+        return logits_per_image, logits_per_text    # 최종 결과물. 두 결과물은 그냥 전치의 차이
